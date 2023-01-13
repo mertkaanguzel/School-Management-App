23af82f5a14e7d47f0e1d9be7f8a3e25d26b9c5d
rtardo <84642031+rtardo@users.noreply.github.com>
2023-01-13 21:42:29 +0300

    lesson module, graphql code first approach

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
+import { LessonModule } from './lesson/lesson.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/local',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson],
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: true,
+      driver: ApolloDriver,
+    }),
+    LessonModule,
+  ],
+})
+export class AppModule {}
---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,27 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Lesson } from './lesson.entity';
+import { CreateLessonInput } from './lesson.input';
+import { LessonService } from './lesson.service';
+import { LessonType } from './lesson.type';
+
+@Resolver((of) => LessonType)
+export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
+
+  @Query((returns) => LessonType)
+  lesson(@Args('id') id: string): Promise<Lesson> {
+    return this.lessonService.getLesson(id);
+  }
+
+  @Query((returns) => [LessonType])
+  lessons(): Promise<Lesson[]> {
+    return this.lessonService.getLessons();
+  }
+
+  @Mutation((returns) => LessonType)
+  createLesson(
+    @Args('createLessonInput') createLessonInput: CreateLessonInput,
+  ): Promise<Lesson> {
+    return this.lessonService.createLesson(createLessonInput);
+  }
+}
